cbba6d1074fff553d04b6051ba52d26f8c610174
keraattin <kerem-oruc@outlook.com>
2023-07-08 23:14:00 +0300

    User Info seperated

---
+++ Windows/Modules/user_info.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Libraries
+##############################################################################
+import subprocess
+##############################################################################
+
+# Functions
+##############################################################################
+def get_user_info():
+    """
+    Retrieve information about the user accounts on the system.
+    """
+    user_info = []
+
+    try:
+        output = subprocess.check_output(
+            'wmic useraccount get name,sid', 
+            shell=True, 
+            universal_newlines=True
+        )
+        lines = output.strip().split('\n')
+        for line in lines[1:]:
+            values = line.strip().split()
+            if len(values) == 2:
+                name, sid = values
+                user_info.append({
+                    'name': name,
+                    'sid': sid
+                })
+    except subprocess.CalledProcessError:
+        pass
+
+    return user_info
+##############################################################################
