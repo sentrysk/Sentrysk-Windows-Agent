e161816fd7cc52f0b6c47ae5a77e1987d4253e3d
keraattin <kerem-oruc@outlook.com>
2023-07-14 20:21:19 +0300

    Fix NPM info

--- Windows/Modules/npm_info.py
+++ Windows/Modules/npm_info.py
@@ -28,29 +28,29 @@ TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 # Functions
 ##############################################################################
-def check_npm_and_collect_packages():
+def get_npm_packages():
     try:
         # Check if npm is installed
         subprocess.run('npm -v', capture_output=True, check=True, shell=True)
 
         # NPM is installed, collect packages and versions
         completed_process = subprocess.run(
-            'npm list --depth=0 --json safas', 
+            'npm list --depth=0 --json', 
             capture_output=True, 
             check=True, 
             shell=True
         )
         package_list = json.loads(completed_process.stdout)['dependencies']
         
-        npm_data = json.loads('{"npm":{"installed":true, "packages":""} }')
+        npm_data = json.loads('{"installed":true, "packages":""}')
         package_data = []
         for package_name, package_info in package_list.items():
             package = {"name": package_name, "version": package_info['version']}
             package_data.append(package)
 
-        npm_data["npm"]["packages"] = package_data
+        npm_data["packages"] = package_data
 
-        return json.dumps(npm_data, indent=4)
+        return npm_data
     except subprocess.CalledProcessError as e:
         # Log the error with date and time
         timestamp = datetime.now().strftime(TIME_FORMAT)
@@ -62,10 +62,8 @@ def check_npm_and_collect_packages():
     except FileNotFoundError:
         # Log the error with date and time
         timestamp = datetime.now().strftime(TIME_FORMAT)
-        return json.dumps(
-            {
-                "npm":{
-                    "installed":False
-                }
-        })
+        data = {
+            "installed":False
+        }
+        return data
 ##############################################################################
