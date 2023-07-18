9273aecd76a1a88cd4d5eb932098a02d73c8bbed
keraattin <kerem-oruc@outlook.com>
2023-07-18 21:18:29 +0300

    Linux User Info Updated

--- Linux/Modules/user_info.py
+++ Linux/Modules/user_info.py
@@ -2,41 +2,28 @@
 
 # Libraries
 ##############################################################################
-import subprocess
+
 ##############################################################################
 
 # Functions
 ##############################################################################
-def get_users():
+def get_user_info():
     try:
-        # Execute the 'getent' command to retrieve the list of users
-        result = subprocess.run(
-            ['getent', 'passwd'], 
-            capture_output=True, 
-            text=True
-        )
-
-        # Check the command's exit status
-        if result.returncode == 0:
-            # Parse the output and extract user information
-            user_list = result.stdout.strip().split('\n')
+        with open('/etc/passwd', 'r') as passwd_file:
             users = []
-            for user_info in user_list:
-                user_data = user_info.split(':')
+            for line in passwd_file:
+                user_info = line.strip().split(':')
                 user = {
-                    'username': user_data[0],
-                    'uid': user_data[2],
-                    'gid': user_data[3],
-                    'home_directory': user_data[5],
-                    'shell': user_data[6]
+                    'username': user_info[0],
+                    'uid': user_info[2],
+                    'gid': user_info[3],
+                    'home_directory': user_info[5],
+                    'shell': user_info[6]
                 }
                 users.append(user)
             
             return users
-        else:
-            print(f"Error: {result.stderr}")
-            return []
     except FileNotFoundError:
-        print("The 'getent' command is not available on your system.")
+        print("The /etc/passwd file does not exist.")
         return []
 ##############################################################################
