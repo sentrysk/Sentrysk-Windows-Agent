9cba02de519dd00695e8ec313615004e4f23e3fc
keraattin <kerem-oruc@outlook.com>
2023-07-31 21:15:00 +0300

    Flask MongoDB connection established

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -3,14 +3,31 @@
 # Libraries
 ##############################################################################
 from flask import Flask
+from mongoengine import connect
+from configparser import ConfigParser
 ##############################################################################
 
 # Configs
 ##############################################################################
+
+# Config File
+CONFIG_FILE = 'config.ini'
+config = ConfigParser()
+config.read(CONFIG_FILE)
+
+# App Config
 HOST = '0.0.0.0'
 PORT = 5000
-
 app = Flask(__name__)
+
+# DB Config
+db = connect(
+    host     = config.get('database','host'),
+    db       = config.get('database','db'),
+    username = config.get('database','username'), 
+    password = config.get('database','password')
+)
+
 ##############################################################################
 
 # Routes
@@ -20,8 +37,26 @@ def hello_world():
     return 'Hello, World!'
 ##############################################################################
 
+# Functions
+def test_mongodb():
+##############################################################################
+    try:
+        db = connect(
+            host     = config.get('database','host'),
+            db       = config.get('database','db'),
+            username = config.get('database','username'), 
+            password = config.get('database','password')
+        )
+        db.server_info() # This will raise an exception if the connection fail
+        print('MongoDB connection successful!')
+    except Exception as e:
+        print(f'MongoDB connection failed: {e}')
+##############################################################################
+
+
 # Main
 ##############################################################################
 if __name__ == '__main__':
+    test_mongodb()
     app.run(host=HOST, port=PORT, debug=True)
 ##############################################################################
