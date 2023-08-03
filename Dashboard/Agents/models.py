42e1229d2f8e1fa9f754a2e1bf6394ca0d100b9c
keraattin <kerem-oruc@outlook.com>
2023-08-03 18:06:13 +0300

    Serializing Added

--- Dashboard/Agents/models.py
+++ Dashboard/Agents/models.py
@@ -2,7 +2,8 @@
 
 # Libraries
 ##############################################################################
-from mongoengine import Document,StringField
+from mongoengine import Document,StringField, DateTimeField
+from datetime import datetime
 ##############################################################################
 
 # Configs
@@ -14,6 +15,18 @@ from mongoengine import Document,StringField
 # Models
 ##############################################################################
 class Agent(Document):
-    type  = StringField(required=True) # Agent type E.g Linux, Windows
-    token = StringField(required=True, unique=True)
+    type    = StringField(required=True) # Agent type E.g Linux, Windows
+    token   = StringField(required=True, unique=True)
+    created = DateTimeField(default=datetime.now)
+
+    def serialize(self):
+        return {
+            "id":str(self.id),
+            "type":self.type,
+            "token":self.token,
+            "created":self.created
+        }
+
+    def __str__(self):
+        return str(self.serialize())
 ##############################################################################
