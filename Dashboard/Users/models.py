891fd5960f79d6f858b6b5bbfae8c0bf2ebc6f97
keraattin <kerem-oruc@outlook.com>
2023-08-06 20:47:44 +0300

    Serialize added to Users model

--- Dashboard/Users/models.py
+++ Dashboard/Users/models.py
@@ -18,4 +18,15 @@ class User(Document):
     email     = StringField(required=True, unique=True)
     password  = BinaryField(required=True)
     created   = DateTimeField(default=datetime.now)
+
+    def serialize(self):
+        return {
+            "id":str(self.id),
+            "email":self.type,
+            "password":self.token,
+            "created":self.created
+        }
+
+    def __str__(self):
+        return str(self.serialize())
 ##############################################################################
