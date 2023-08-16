05b993babdbb751d201af7ee23095eb0286cbd6c
keraattin <kerem-oruc@outlook.com>
2023-08-16 16:00:20 +0300

    Logging config added to agent

--- Linux/agent.py
+++ Linux/agent.py
@@ -3,7 +3,8 @@
 # Libraries
 ##############################################################################
 import json
-from configparser import ConfigParser
+from configparser import ConfigParser,ExtendedInterpolation
+import logging
 
 from Modules.system_info import get_system_info
 from Modules.user_info import get_user_info
@@ -18,10 +19,21 @@ from Modules.docker_info import get_docker_info
 ##############################################################################
 CONFIG_FILE = 'config.ini'
 
-config = ConfigParser()
+config = ConfigParser(interpolation=ExtendedInterpolation())
 config.read(CONFIG_FILE)
 
-DASHBOARD_URL = config.get('dashboard','url')
+DASHBOARD_URL   = config.get('dashboard','url')
+LOGFILE         = config.get('logging','logfile')
+
+
+FORMAT = '%(asctime)s :: %(levelname)-6s :: %(name)s :: [%(filename)s:%(lineno)s - %(funcName)s()] :: %(message)s'
+# Configure logging to write logs to a log file
+logging.basicConfig(
+    filename=LOGFILE, 
+    level=logging.INFO,
+    format=FORMAT,
+    encoding='utf-8'
+)
 ##############################################################################
 
 # Functions
