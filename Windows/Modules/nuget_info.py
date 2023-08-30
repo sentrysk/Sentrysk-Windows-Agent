d09b3f6fe72be369e92a7d0ee081c673e788925b
keraattin <kerem-oruc@outlook.com>
2023-08-30 23:41:29 +0300

    Logging added to Windows/nuget_info.py

--- Windows/Modules/nuget_info.py
+++ Windows/Modules/nuget_info.py
@@ -1,15 +1,23 @@
+#!/usr/bin/env python3
+
+# Libraries
+##############################################################################
 import subprocess
 import json
+import logging
+##############################################################################
 
+# Functions
+##############################################################################
 def list_installed_packages():
     try:
         result = subprocess.run(["nuget", "list", "-AllVersions", "-Prerelease", "-Source", "https://api.nuget.org/v3/index.json", "--outputformat", "json"], capture_output=True, text=True, check=True)
         packages_data = json.loads(result.stdout)
         return packages_data
     except Exception as e:
-        return {"error": "NuGet command failed", "message": e}
-
-if __name__ == "__main__":
-    result = list_installed_packages()
-    print(result)
-    #print(json.dumps(result, indent=2))
+        # Log the error
+        logging.error(e)
+        return {
+            "running": "false"
+        }
+##############################################################################
