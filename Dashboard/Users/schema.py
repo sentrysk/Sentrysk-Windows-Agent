46ec448a0966751f34dc9cacaf0f08dd40665daf
keraattin <kerem-oruc@outlook.com>
2023-09-08 10:39:31 +0300

    Register schema updated

--- Dashboard/Users/schema.py
+++ Dashboard/Users/schema.py
@@ -2,12 +2,18 @@
 
 # Libraries
 ##############################################################################
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 ##############################################################################
 
 # Schemas
 ##############################################################################
 class RegisterSchema(Schema):
+    name       = fields.Str(required = True)
+    lastname   = fields.Str(required = True)
+    email      = fields.Email(required = True)
+    password   = fields.Str(required = True)
+
+class LoginSchema(Schema):
     email      = fields.Email(required = True)
     password   = fields.Str(required = True)
 ##############################################################################
