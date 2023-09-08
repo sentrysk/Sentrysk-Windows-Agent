01bc6163af25ca144dfe59fd567907f3a464eeaa
keraattin <kerem-oruc@outlook.com>
2023-09-08 10:14:04 +0300

    User registration validation added

--- Dashboard/Users/routes.py
+++ Dashboard/Users/routes.py
@@ -7,10 +7,12 @@ import bcrypt
 import jwt
 from datetime import datetime,timedelta
 from configparser import ConfigParser
+from marshmallow import  ValidationError
 
 from .models import User
 from Session.models import Session
 from .validators import auth_token_required
+from .schema import RegisterSchema
 ##############################################################################
 
 # Blueprint
@@ -33,19 +35,26 @@ JWT_ALG = 'HS256' # JWT Algorithm
 ##############################################################################
 @users_bp.route('/register', methods=['POST'])
 def register_user():
-    data = request.get_json()
+    try:
+        # Load and validate the JSON request using the schema
+        data = RegisterSchema().load(request.json)
+    except ValidationError as e:
+        # Return validation errors as a JSON response with a 400 status code
+        return jsonify({'error': e.messages}), 400
+
+    # Get Email & Password from request data
     email = data.get('email')
     password = data.get('password')
 
-    if not email or not password:
-        return jsonify({'message': 'Email and password are required.'}), 400
-
+    # Check if User already exist
     existing_user = User.objects(email=email).first()
     if existing_user:
         return jsonify({'message': 'Email already registered.'}), 409
 
+    # Generate password hash
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+    # Register User
     user = User(email=email, password=hashed_password)
     user.save()
 
