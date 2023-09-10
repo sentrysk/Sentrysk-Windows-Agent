987aca4ee7c156b97fab0ffbf833f74c18fe68da
keraattin <kerem-oruc@outlook.com>
2023-09-10 11:17:54 +0300

    Agent token validator added to validators

--- Dashboard/Shared/validators.py
+++ Dashboard/Shared/validators.py
@@ -7,6 +7,7 @@ from flask import request, jsonify
 import jwt
 
 from Session.models import Session
+from Agents.models import Agent
 from .configs import SECRET_KEY,JWT_ALG
 ##############################################################################
 
@@ -38,6 +39,36 @@ def auth_token_required(f):
                     algorithms=[JWT_ALG]
                 )
             else:
+                return jsonify(
+                    {
+                        'message':'Token is invalid'
+                    }
+                ),498
+        except Exception as e:
+            return jsonify(
+                {
+                    'message':'Token is invalid'
+                }
+            ),498
+        
+        return f(*args, **kwargs)
+    return decorated
+
+def agent_token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth_header = request.headers.get('Authorization')
+
+        if not auth_header:
+            return jsonify(
+                {
+                    'message':'Token is missing'
+                }
+            ),499
+
+        try:
+            agent = Agent.objects(token=auth_header).first()
+            if not agent:
                 return jsonify(
                     {
                         'message':'Token is invalid'
