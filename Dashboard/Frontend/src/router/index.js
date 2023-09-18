69e1142749f782794bb5d381622247d4259e486e
keraattin <kerem-oruc@outlook.com>
2023-09-18 17:24:49 +0300

    Routes protection from unauthenticated users added

--- Dashboard/Frontend/src/router/index.js
+++ Dashboard/Frontend/src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginPage from '../components/LoginPage.vue'
+import axios from "axios";
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: { requiresAuth: true }
   },
   {
     path: '/login',
@@ -28,4 +30,44 @@ const router = createRouter({
   routes
 })
 
-export default router
+// Add a navigation guard to check session validity
+router.beforeEach(async (to, from, next) => {
+  // Check if the route requires authentication
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // Check if the user is logged in
+    const jwtToken = sessionStorage.getItem("jwtToken");
+
+    if (jwtToken) {
+      // User is authenticated, proceed to check the session
+      try {
+        // Send a GET request to the session check endpoint
+        const response = await axios.get("http://localhost:5000/session/check", {
+          headers: {
+            Authorization: jwtToken,
+          },
+        });
+
+        // Check if the session check is successful (e.g., HTTP status 200)
+        if (response.status === 200) {
+          // Session is valid, allow access to the route
+          next();
+        } else {
+          // Session check failed, redirect to the login page
+          next("/login");
+        }
+      } catch (error) {
+        console.error("Session check failed:", error);
+        // Handle session check error, redirect to login or display an error message
+        next("/login");
+      }
+    } else {
+      // User is not authenticated, redirect to the login page
+      next("/login");
+    }
+  } else {
+    // Route doesn't require authentication, allow access
+    next();
+  }
+});
+
+export default router;
