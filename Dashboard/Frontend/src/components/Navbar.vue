8a286cb6772a2d6630e0ee05d24eb3799e466684
keraattin <kerem-oruc@outlook.com>
2023-09-18 17:51:28 +0300

    Navbar added

---
+++ Dashboard/Frontend/src/components/Navbar.vue
@@ -0,0 +1,64 @@
+<template>
+    <nav class="navbar">
+    <div class="navbar-brand">
+      <router-link to="/">Home</router-link>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <button @click="logout">Logout</button>
+        </div>
+      </div>
+    </div>
+  </nav>
+  </template>
+  
+  <script>
+  import Swal from "sweetalert2";
+  
+  export default {
+    // ... Your other component options
+  
+    methods: {
+      async logout() {
+        // Get the JWT token from sessionStorage
+        const jwtToken = sessionStorage.getItem("jwtToken");
+  
+        if (jwtToken) {
+          try {
+            // Send a POST request to your logout endpoint (if your backend has one)
+            // This step is optional and depends on your backend's logout implementation
+            // Example: await axios.post("YOUR_LOGOUT_API_URL", {}, { headers: { Authorization: `Bearer ${jwtToken}` } });
+  
+            // Clear the JWT token and session expiration from sessionStorage
+            sessionStorage.removeItem("jwtToken");
+            sessionStorage.removeItem("tokenExpiration");
+  
+            // Show a logout success message with SweetAlert
+            Swal.fire({
+              icon: "success",
+              title: "Logged out",
+              text: "You have been successfully logged out.",
+            });
+  
+            // Redirect to the login page
+            this.$router.push("/login"); // You can change "/login" to the actual login route
+          } catch (error) {
+            console.error("Logout failed:", error);
+  
+            // Handle logout error and show an error message with SweetAlert
+            Swal.fire({
+              icon: "error",
+              title: "Logout error",
+              text: "An error occurred during the logout process.",
+            });
+          }
+        } else {
+          // User is not authenticated (no JWT token), redirect to the login page
+          this.$router.push("/login"); // You can change "/login" to the actual login route
+        }
+      },
+    },
+  };
+  </script>
+  
