d155aca1285c4573fbb25fb54670842765885325
keraattin <kerem-oruc@outlook.com>
2023-09-19 11:29:12 +0300

    Login page style changed

--- Dashboard/Frontend/src/components/LoginPage.vue
+++ Dashboard/Frontend/src/components/LoginPage.vue
@@ -1,15 +1,46 @@
 <template>
-    <div class="login-container">
-      <h1>Login</h1>
-      <form @submit.prevent="login">
-        <input type="text" v-model="email" placeholder="Email" />
-        <input type="password" v-model="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card mt-5 shadow">
+          <div class="card-header bg-dark text-white">
+            <h1 class="text-center">
+              Login
+            </h1>
+          </div>
+          <div class="card-body">
+            <form @submit.prevent="login">
+              <div class="mb-3 input-group">
+                <span class="input-group-text"><i class="bi bi-envelope"></i></span>
+                <input type="email" v-model="email" class="form-control" placeholder="Email" required />
+              </div>
+              <div class="mb-4 input-group">
+                <span class="input-group-text"><i class="bi bi-lock"></i></span>
+                <input
+                  :type="showPassword ? 'text' : 'password'"
+                  v-model="password"
+                  class="form-control"
+                  placeholder="Password"
+                  required
+                />
+                <button type="button" class="btn btn-outline-secondary" @click="togglePasswordVisibility">
+                  <i :class="['bi', showPassword ? 'bi-eye-slash' : 'bi-eye']"></i>
+                </button>
+              </div>
+              <button type="submit" class="btn btn-primary btn-block">Login</button>
+            </form>
+            <!-- Add registration link here -->
+            <div class="text-center mt-3">
+              <router-link to="/register">Don't have an account? Register here.</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
+<script>
   import axios from "axios";
   import Swal from "sweetalert2"; // Import SweetAlert2
 
@@ -64,28 +95,54 @@
       },
     },
   };
-  </script>
+</script>
   
-  <style scoped>
-  .login-container {
-    max-width: 400px;
-    margin: 0 auto;
-    padding: 20px;
-    text-align: center;
-  }
-  
-  input {
-    width: 100%;
-    padding: 10px;
-    margin: 10px 0;
-  }
-  
-  button {
-    background-color: #007bff;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-    cursor: pointer;
-  }
-  </style>
-  
+<style scoped>
+.shadow {
+  box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.2);
+}
+
+.input-group {
+  margin-bottom: 15px;
+}
+
+.input-group-text {
+  background-color: #343a40; /* Darker background color */
+  border: none;
+  color: white; /* White text color */
+}
+
+.bi {
+  font-size: 1.5rem;
+  color: #007bff; /* Blue icon color */
+}
+
+.btn-outline-secondary {
+  background-color: #343a40; /* Darker background color */
+  border: none;
+  color: white; /* White text color */
+}
+
+.btn-outline-secondary:hover {
+  background-color: #292e33; /* Slightly darker on hover */
+}
+
+.btn-primary {
+  background-color: #007bff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px;
+}
+
+.btn-primary:hover {
+  background-color: #0056b3;
+}
+
+.text-center {
+  text-align: center;
+}
+
+.mb-4 {
+  margin-bottom: 1.5rem;
+}
+</style>
