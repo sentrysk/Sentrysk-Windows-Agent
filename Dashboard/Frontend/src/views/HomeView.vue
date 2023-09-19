93ea4a2d1eed00d577ad1d54fc4b52d006f467fa
keraattin <kerem-oruc@outlook.com>
2023-09-19 11:33:48 +0300

    Home view style changed

--- Dashboard/Frontend/src/views/HomeView.vue
+++ Dashboard/Frontend/src/views/HomeView.vue
@@ -1,5 +1,14 @@
 <template>
   <Navbar /> <!-- Include the Navbar component here -->
+  <div class="container mt-5">
+    <div class="jumbotron">
+      <h1 class="display-4">Welcome to Our App</h1>
+      <p class="lead">This is a beautiful home view created with Vue.js and Bootstrap.</p>
+      <hr class="my-4">
+      <p>Enjoy exploring our app's features and functionality.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+  </div>
 </template>
 
 <script>
