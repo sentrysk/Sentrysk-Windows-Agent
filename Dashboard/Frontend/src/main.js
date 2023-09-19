fcdda6bdef83cf1e3fe5f85fefcfa1f666a3d204
keraattin <kerem-oruc@outlook.com>
2023-09-19 15:31:59 +0300

    Fontawesome added

--- Dashboard/Frontend/src/main.js
+++ Dashboard/Frontend/src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 createApp(App).use(router).mount('#app')
