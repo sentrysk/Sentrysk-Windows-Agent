5ed4088e85cf0d23043767e9c2b57abf3f20a46e
keraattin <kerem-oruc@outlook.com>
2023-09-28 12:08:11 +0300

    register endpoint converted to Create or Update

--- Dashboard/Backend/SystemInfo/routes.py
+++ Dashboard/Backend/SystemInfo/routes.py
@@ -6,8 +6,8 @@ from flask import Blueprint, request, jsonify
 import json
 from marshmallow import ValidationError
 
-from .models import SystemInfo
-from .schema import RegisterSchema
+from .models import SystemInfo, ChangeLogSystemInfo
+from .schema import RegisterSchema,UpdateSchema
 from Shared.validators import agent_token_required
 
 from Agents.helper_funcs import get_id_by_token
@@ -27,20 +27,6 @@ sys_info_bp = Blueprint('sys_info_blueprint', __name__)
 @sys_info_bp.route('/', methods=['POST'])
 @agent_token_required
 def register():
-    try:
-        # Load and validate the JSON request using the schema
-        data = RegisterSchema().load(request.json)
-    except ValidationError as e:
-        # Return validation errors as a JSON response with a 400 status code
-        return jsonify({'error': e.messages}), 400
-    
-    os                  = data.get('os')
-    domain              = data.get('domain')
-    cpu                 = data.get('cpu')
-    memory              = data.get('memory')
-    disks               = data.get('disks')
-    network_interfaces  = data.get('network_interfaces')
-
     # Get Agent Token by Authorization Header
     agent_token = request.headers.get('Authorization')
     # Get Agent ID by Token
@@ -48,17 +34,68 @@ def register():
     # Get Agent by Agent ID
     agent = Agent.objects(id=agent_id).first()
 
-    # Register System Info to DB
-    sys_info = SystemInfo(
-        agent = agent,
-        os = os, 
-        domain=domain,
-        cpu = cpu,
-        memory = memory,
-        disks = disks,
-        network_interfaces = network_interfaces
-    )
-    sys_info.save()
+    # Get if record already exist
+    sys_info = SystemInfo.objects(agent=agent).first()
+
+    if sys_info:
+        # UPDATE If System Information data already exist 
+        
+        # Get Data and Validate Request
+        try:
+            # Load and validate the JSON request using the schema
+            data = UpdateSchema().load(request.json)
+        except ValidationError as e:
+            # Return validation errors as a JSON response with a 400 status code
+            return jsonify({'error': e.messages}), 400
+        
+        changes = {}
+
+        for field, new_value in data.items():
+            if field in sys_info._data and sys_info[field] != new_value:
+                changes[field] = {
+                    'previous_value': sys_info[field],
+                    'new_value': new_value
+                }
+
+        # If any changes
+        if changes:
+            # Update the existing SystemInfo document
+            sys_info.update(**data)
+            
+            # Create a new ChangeLog entry
+            change_log_entry = ChangeLogSystemInfo(
+                changes = changes
+            )
+            change_log_entry.save()
+    else:
+        # CREATE If System Information not exist 
+        
+        # Get Data and Validate Request
+        try:
+            # Load and validate the JSON request using the schema
+            data = RegisterSchema().load(request.json)
+        except ValidationError as e:
+            # Return validation errors as a JSON response with a 400 status code
+            return jsonify({'error': e.messages}), 400
+
+        os                  = data.get('os')
+        domain              = data.get('domain')
+        cpu                 = data.get('cpu')
+        memory              = data.get('memory')
+        disks               = data.get('disks')
+        network_interfaces  = data.get('network_interfaces')
+
+        # Register System Info to DB
+        sys_info = SystemInfo(
+            agent = agent,
+            os = os, 
+            domain=domain,
+            cpu = cpu,
+            memory = memory,
+            disks = disks,
+            network_interfaces = network_interfaces
+        )
+        sys_info.save()
     
     sys_info_data = json.loads(sys_info.to_json())
 
@@ -67,6 +104,6 @@ def register():
             'message': 'System info registered successfully.',
             'sys_info': sys_info_data,
         }
-    ), 201
+    ), 200
 
 ##############################################################################
