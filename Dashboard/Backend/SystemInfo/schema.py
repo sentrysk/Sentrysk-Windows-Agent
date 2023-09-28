e7f7da48b4bd4a68fcdb270aef70cf62fbb0b8a7
keraattin <kerem-oruc@outlook.com>
2023-09-28 12:07:11 +0300

    Update schema added for System Info Data

--- Dashboard/Backend/SystemInfo/schema.py
+++ Dashboard/Backend/SystemInfo/schema.py
@@ -8,6 +8,14 @@ from marshmallow import Schema, fields
 # Schemas
 ##############################################################################
 class RegisterSchema(Schema):
+    os                  = fields.Raw(required=True)
+    domain              = fields.Raw(required=True)
+    cpu                 = fields.Raw(required=True)
+    memory              = fields.Raw(required=True)
+    disks               = fields.Raw(required=True)
+    network_interfaces  = fields.Raw(required=True)
+
+class UpdateSchema(Schema):
     os                  = fields.Raw()
     domain              = fields.Raw()
     cpu                 = fields.Raw()
