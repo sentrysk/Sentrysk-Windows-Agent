03895e72a439e3af064c9478dfd25fd01d8d5ca6
keraattin <kerem-oruc@outlook.com>
2023-09-28 12:07:31 +0300

    Changelog model added for System Info

--- Dashboard/Backend/SystemInfo/models.py
+++ Dashboard/Backend/SystemInfo/models.py
@@ -3,9 +3,11 @@
 # Libraries
 ##############################################################################
 from mongoengine import (
-    Document,  ListField, DictField, ReferenceField
+    Document,  ListField, DictField, ReferenceField,  DateTimeField,
+    StringField
 )
 from Agents.models import Agent
+from datetime import datetime
 ##############################################################################
 
 ##############################################################################
@@ -17,4 +19,8 @@ class SystemInfo(Document):
     memory              = DictField()
     disks               = ListField(DictField())
     network_interfaces  = ListField(DictField())
+
+class ChangeLogSystemInfo(Document):
+    timestamp = DateTimeField(default=datetime.utcnow)
+    changes = DictField()
 ##############################################################################
