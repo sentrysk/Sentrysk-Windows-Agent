ecefead9ede5b75111fbff72ff2f5cc48537c2fa
keraattin <kerem-oruc@outlook.com>
2023-09-29 17:10:49 +0300

    CPU fix & Network Interfaces converted to dict

--- MacOs/Modules/system_info.py
+++ MacOs/Modules/system_info.py
@@ -24,18 +24,18 @@ def get_system_info():
     }
 
     # Collect OS information
-    system_info["os"]["name"] = platform.system()
-    system_info["os"]["version"] = platform.mac_ver()[0]
+    system_info["os"]["system"] = platform.system()
+    system_info["os"]["version"] = platform.mac_ver()[0] # Os Release Version
+    system_info["os"]["kernel"] = platform.release() # Kernel
 
     # Collect domain/host information
     system_info["domain"]["hostname"] = platform.node()
     system_info["domain"]["fqdn"] = platform.node()
 
     # Collect CPU information
-    cpu_info = dict(psutil.cpu_info()._asdict())
     system_info["cpu"]["cpu_count"] = psutil.cpu_count(logical=False)
     system_info["cpu"]["cpu_threads"] = psutil.cpu_count(logical=True)
-    system_info["cpu"]["cpu_model"] = cpu_info["brand"]
+    system_info["cpu"]["cpu_model"] = platform.processor()
     system_info["cpu"]["cpu_architecture"] = platform.machine()
 
     # Collect memory information
@@ -59,25 +59,19 @@ def get_system_info():
     system_info["disks"] = disk_info
 
     # Collect network interface information including MAC addresses
-    network_info = []
     for interface, addrs in psutil.net_if_addrs().items():
-        network = {
-            "interface_name": interface,
-            "addresses": []
+        system_info["network_interfaces"][interface] = {
+            "mac_address": ""
         }
         for addr in addrs:
-            address = {
+            system_info["network_interfaces"][interface][addr.family.name] = {
                 "family": addr.family.name,
                 "address": addr.address,
                 "netmask": addr.netmask,
             }
-            network["addresses"].append(address)
         
         # Add MAC address to the network interface information
-        network_info.append(network)
-        network_info[-1]["mac_address"] = psutil.net_if_addrs()[interface][0].address
-
-    system_info["network_interfaces"] = network_info
+        system_info["network_interfaces"][interface]["mac_address"] = psutil.net_if_addrs()[interface][0].address
 
     # Print the JSON result
     return system_info
