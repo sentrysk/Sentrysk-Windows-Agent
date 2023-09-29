0a8e6040efa8c113e407e961ddc971f0db7d9cac
keraattin <kerem-oruc@outlook.com>
2023-09-29 15:38:03 +0300

    System info implemented

--- MacOs/agent.py
+++ MacOs/agent.py
@@ -6,6 +6,7 @@ import json
 from configparser import ConfigParser,ExtendedInterpolation
 import logging
 
+from Modules.system_info import get_system_info
 ##############################################################################
 
 # Configs
@@ -36,4 +37,12 @@ logging.basicConfig(
 """
 
 system_info = {}
+
+system_info['system']               = get_system_info()
 ##############################################################################
+
+# Usage
+json_data = json.dumps(system_info, indent=4)
+with open('result.json','w') as f:
+    f.write(json_data)
+print(json_data)
