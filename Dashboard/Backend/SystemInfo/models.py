8cc7d28b69dbafe575a0a56ba4c96f6ed7b0d689
keraattin <kerem-oruc@outlook.com>
2023-10-01 14:07:12 +0300

    SystemInfo model serializing added

--- Dashboard/Backend/SystemInfo/models.py
+++ Dashboard/Backend/SystemInfo/models.py
@@ -3,8 +3,7 @@
 # Libraries
 ##############################################################################
 from mongoengine import (
-    Document,  ListField, DictField, ReferenceField,  DateTimeField,
-    StringField
+    Document, DictField, ReferenceField,  DateTimeField
 )
 from Agents.models import Agent
 from datetime import datetime
@@ -19,6 +18,21 @@ class SystemInfo(Document):
     memory              = DictField()
     disks               = DictField()
     network_interfaces  = DictField()
+
+    def serialize(self):
+        return {
+            "id":str(self.id),
+            "agent_id":str(self.agent.id),
+            "os":self.os,
+            "domain":self.domain,
+            "cpu":self.cpu,
+            "memory":self.memory,
+            "disks":self.disks,
+            "network_interfaces":self.network_interfaces
+        }
+
+    def __str__(self):
+        return str(self.serialize())
 
 class ChangeLogSystemInfo(Document):
     timestamp = DateTimeField(default=datetime.utcnow)
