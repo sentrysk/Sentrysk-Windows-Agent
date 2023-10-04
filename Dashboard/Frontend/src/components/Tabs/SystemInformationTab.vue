d61129f44992de983927109e09a10c0ddc3384de
keraattin <kerem-oruc@outlook.com>
2023-10-04 09:23:18 +0300

    Last Update time added

--- Dashboard/Frontend/src/components/Tabs/SystemInformationTab.vue
+++ Dashboard/Frontend/src/components/Tabs/SystemInformationTab.vue
@@ -20,6 +20,11 @@
         </li>
     </ul>
 
+    <!-- Last Update -->
+    <div class="row">
+        <span :title=formatToLocalTime(systemInfo.updated)>Last Update : {{ calculateDatetimeDifference(systemInfo.updated) }}</span>
+    </div>
+
     <div class="tab-content" id="myTabContent">
         <!-- OS Tab -->
         <div class="tab-pane fade show active" id="systemOs" role="tabpanel" aria-labelledby="systemOs">
@@ -204,7 +209,53 @@
             console.error('Error fetching agents:', error);
           }
         },
-      }
+        formatToLocalTime(utcTime) {
+            // Create a Date object from the UTC time
+            const utcDate = new Date(utcTime);
+
+            // Format the date and time to the user's locale and timezone
+            const formattedTime = utcDate.toLocaleString(undefined, {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            timeZoneName: 'short',
+            hour12: false,
+            });
+
+            return formattedTime;
+        },
+        // Function to calculate the difference between two datetime values
+        calculateDatetimeDifference(datetime1) {
+            // Parse the datetime strings into Date objects
+            const date1 = new Date(datetime1);
+            const date2 = new Date();
+
+            // Calculate the time difference in milliseconds
+            const timeDifference = Math.abs(date1 - date2);
+
+            // Calculate the difference in days, hours, minutes, and seconds
+            const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+            
+            if (seconds > 0){
+                if(minutes > 0){
+                    if(hours > 0){
+                        if(days > 0){
+                            return days.toString()+" days "+hours.toString()+" hours "+minutes.toString()+" minutes "+seconds.toString()+" seconds ago"
+                        }
+                        return hours.toString()+" hours "+minutes.toString()+" minutes "+seconds.toString()+" seconds ago"
+                    }
+                    return minutes.toString()+" minutes "+seconds.toString()+" seconds ago"
+                }
+                return seconds.toString()+" seconds ago"
+            }
+        }
+      },
     };
 </script>
 
