289fcc221d5a7a5e34a29f765f82a1ce4a4aefa7
keraattin <kerem-oruc@outlook.com>
2023-10-24 12:56:22 +0300

    Installed Apps Model Updated

--- Dashboard/Backend/SystemInstalledApps/models.py
+++ Dashboard/Backend/SystemInstalledApps/models.py
@@ -3,7 +3,7 @@
 # Libraries
 ##############################################################################
 from mongoengine import (
-    Document, DictField, ReferenceField,  DateTimeField,  ListField,
+    Document, ReferenceField, DateTimeField, ListField,
     EmbeddedDocument, EmbeddedDocumentField, StringField
 )
 from Agents.models import Agent
@@ -21,7 +21,6 @@ class InstalledApp(EmbeddedDocument):
     version         = StringField()
     installed_by    = StringField()
 
-
     def serialize(self):
         data = {
             "username": self.username,
@@ -31,6 +30,22 @@ class InstalledApp(EmbeddedDocument):
             data["installed_by"] = self.installed_by
 
         return data
+
+    def __str__(self):
+        return str(self.serialize())
+
+class SystemInstalledApps(Document):
+    agent     = ReferenceField(Agent)
+    apps      = ListField(EmbeddedDocumentField(InstalledApp))
+    updated   = DateTimeField(default=datetime.utcnow)
+    
+    def serialize(self):
+        return {
+            "id":str(self.id),
+            "agent_id":str(self.agent.id),
+            "apps":self.apps,
+            "updated":self.updated
+        }
 
     def __str__(self):
         return str(self.serialize())
