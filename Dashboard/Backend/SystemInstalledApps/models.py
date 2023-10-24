70d954090664a099568acc814bb8178fdad04ca1
keraattin <kerem-oruc@outlook.com>
2023-10-24 12:54:41 +0300

    Installed Apps Model Created

---
+++ Dashboard/Backend/SystemInstalledApps/models.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Libraries
+##############################################################################
+from mongoengine import (
+    Document, DictField, ReferenceField,  DateTimeField,  ListField,
+    EmbeddedDocument, EmbeddedDocumentField, StringField
+)
+from Agents.models import Agent
+from datetime import datetime
+##############################################################################
+
+# Global Values
+##############################################################################
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+##############################################################################
+
+##############################################################################
+class InstalledApp(EmbeddedDocument):
+    name            = StringField()
+    version         = StringField()
+    installed_by    = StringField()
+
+
+    def serialize(self):
+        data = {
+            "username": self.username,
+            "version": self.version
+        }
+        if self.installed_by:
+            data["installed_by"] = self.installed_by
+
+        return data
+
+    def __str__(self):
+        return str(self.serialize())
+##############################################################################
