d6009aee03d59866db8ed423144021880bf290b4
keraattin <kerem-oruc@outlook.com>
2023-10-26 14:11:05 +0300

    Compare apps method added

--- Dashboard/Backend/SystemInstalledApps/models.py
+++ Dashboard/Backend/SystemInstalledApps/models.py
@@ -30,7 +30,7 @@ class InstalledApp(EmbeddedDocument):
 
     def serialize(self):
         data = {
-            "username": self.username,
+            "name": self.name,
             "version": self.version
         }
         if self.installed_by:
@@ -45,6 +45,37 @@ class SystemInstalledApps(Document):
     agent     = ReferenceField(Agent)
     apps      = ListField(EmbeddedDocumentField(InstalledApp))
     updated   = DateTimeField(default=datetime.utcnow)
+
+    # To compare 2 App Data
+    def compare_apps(self, other):
+        if not isinstance(other, SystemInstalledApps):
+            raise ValueError("Comparison should be done with another SystemInstalledApps instance")
+
+        self_apps = {app.name: app for app in self.apps}
+        other_apps = {app.name: app for app in other.apps}
+
+        # Newly Added Apps
+        new_apps = [app.serialize() for name, app in other_apps.items() if name not in self_apps]
+        # Deleted Apps
+        deleted_apps = [app.serialize() for name, app in self_apps.items() if name not in other_apps]
+        # Updated Apps and Fields
+        updated_apps = {}
+
+        # Find the Updated Fields
+        for appname in self_apps:
+            if appname in other_apps and self_apps[appname] != other_apps[appname]:
+                updated_fields = {}
+                for field in InstalledApp._fields:
+                    if str(self_apps[appname][field]) != str(other_apps[appname][field]):
+                        updated_fields[field] = {
+                            "previous_value":self_apps[appname][field],
+                            "new_value":other_apps[appname][field]
+                        }
+                # Add Updated Fields to Updated apps Dict
+                updated_apps[appname] = updated_fields
+
+        # Return Compared Data
+        return new_apps, deleted_apps, updated_apps
     
     def serialize(self):
         return {
