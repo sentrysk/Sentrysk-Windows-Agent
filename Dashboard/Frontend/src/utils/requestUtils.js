00433cf6ab78fc13e4e95ee0750fad653221086d
keraattin <kerem-oruc@outlook.com>
2023-11-03 17:55:05 +0300

    get System Users added to request utils

--- Dashboard/Frontend/src/utils/requestUtils.js
+++ Dashboard/Frontend/src/utils/requestUtils.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 // Global Variables
 const API_URL  =  "http://localhost:5000"
 const SYS_INFO_EP = "/sysinfo/"
+const SYS_USR_EP = "/sysusers/"
 const CHLG_EP = "/changelog"
 
-// Retrive System Information Function
+// Retrive System Information
 export async function getSystemInformation(agentId){
     try {
         // Retrieve JWT token from session storage
@@ -22,6 +23,7 @@ export async function getSystemInformation(agentId){
     }
 }
 
+// Retrive System Information Changelog
 export async function getSysInfoChangeLog(sysInfoId){
     try {
         // Retrieve JWT token from session storage
@@ -35,5 +37,22 @@ export async function getSysInfoChangeLog(sysInfoId){
         return changelog.data
     } catch (error) {
         console.error('Error fetching System Information Changelog:', error);
+    }
+}
+
+// Retrive System Users
+export async function getSystemUsers(agentId){
+    try {
+        // Retrieve JWT token from session storage
+        const jwtToken = sessionStorage.getItem('jwtToken');
+        const URL = API_URL + SYS_USR_EP + agentId
+        const response = await axios.get(URL, {
+            headers: {
+                Authorization: jwtToken,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching System Users:', error);
     }
 }
