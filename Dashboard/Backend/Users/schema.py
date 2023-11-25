e91f88da757e2e81bade8171306caf483eb9f0ee
keraattin <kerem-oruc@outlook.com>
2023-11-25 14:56:51 +0300

    Regex added for name in user schema

--- Dashboard/Backend/Users/schema.py
+++ Dashboard/Backend/Users/schema.py
@@ -5,10 +5,15 @@
 from marshmallow import Schema, fields, validate
 ##############################################################################
 
+# Regexs
+##############################################################################
+ALPH_NUM_REGX = r"^[a-zA-Z]{1,25}(?: [a-zA-Z]{1,25})?$"
+##############################################################################
+
 # Schemas
 ##############################################################################
 class RegisterSchema(Schema):
-    name       = fields.Str(required = True)
+    name       = fields.Str(required = True,validate=validate.Regexp(ALPH_NUM_REGX))
     lastname   = fields.Str(required = True)
     email      = fields.Email(required = True)
     password   = fields.Str(required = True)
