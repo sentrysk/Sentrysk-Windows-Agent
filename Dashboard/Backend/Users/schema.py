be598b9ab8443174dbd9a495b47c41953cc2137f
keraattin <kerem-oruc@outlook.com>
2023-11-26 19:07:51 +0300

    Regex Validation added for Lastname field

--- Dashboard/Backend/Users/schema.py
+++ Dashboard/Backend/Users/schema.py
@@ -8,13 +8,14 @@ from marshmallow import Schema, fields, validate
 # Regexs
 ##############################################################################
 NAME_REGX = r'^[\w]{2,30}(?: [\w]{1,30})?$'
+LNAME_REGX = r'^[\w]{2,30}$'
 ##############################################################################
 
 # Schemas
 ##############################################################################
 class RegisterSchema(Schema):
     name       = fields.Str(required = True,validate=validate.Regexp(NAME_REGX))
-    lastname   = fields.Str(required = True)
+    lastname   = fields.Str(required = True,validate=validate.Regexp(LNAME_REGX))
     email      = fields.Email(required = True)
     password   = fields.Str(required = True)
 
