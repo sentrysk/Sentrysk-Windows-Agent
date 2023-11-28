d4b17e76b68d11552ec10c06425dc9f586bc5db3
keraattin <kerem-oruc@outlook.com>
2023-11-28 20:34:32 +0300

    run tests py file created

---
+++ Dashboard/Tests/run.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Libraries
+##############################################################################
+from Modules.Register import *
+##############################################################################
+
+# Register Tests
+##############################################################################
+# Invalid Name Test
+if test_register_invalid_name():
+    print("[SUCCESS]\t[test_register_invalid_name]")
+else:
+    print("[FAIL]\t[test_register_invalid_name]")
+
+# Invalid Lastname Test
+if test_register_invalid_lastname():
+    print("[SUCCESS]\t[test_register_invalid_lastname]")
+else:
+    print("[FAIL]\t[test_register_invalid_lastname]")
+
+# Email Test
+if test_register_invalid_email():
+    print("[SUCCESS]\t[test_register_invalid_email]")
+else:
+    print("[FAIL]\t[test_register_invalid_email]")
+
+# Successfuly Register Test
+if test_register_success():
+    print("[SUCCESS]\t[test_register_success]")
+else:
+    print("[FAIL]\t[test_register_success]")
+##############################################################################
