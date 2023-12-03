eae0ae3b748e37ff75e44d1f2b0ab12a2d47b876
keraattin <kerem-oruc@outlook.com>
2023-12-03 19:27:32 +0300

    Services Changelog Tab Added

--- Dashboard/Frontend/src/components/Tabs/SystemServicesTab.vue
+++ Dashboard/Frontend/src/components/Tabs/SystemServicesTab.vue
@@ -1,10 +1,20 @@
 <template>
     <ul class="nav nav-tabs" id="systemSubTab" role="tablist">
+        <!-- Services Section -->
         <li class="nav-item" role="presentation">
             <button class="nav-link active" id="systemServicesTab" data-bs-toggle="tab" data-bs-target="#systemServices" type="button" role="tab" aria-controls="systemServices" aria-selected="true">
               <i class="bi bi-code-square"></i>Services
               <span class="badge rounded-pill bg-primary">
               {{ systemServicesCount }}
+              </span>
+            </button>
+        </li>
+        <!-- Changelog Section -->
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="systemServicesChangelogTab" data-bs-toggle="tab" data-bs-target="#systemServicesChangelog" type="button" role="tab" aria-controls="systemServicesChangelog" aria-selected="false">
+              <i class="bi bi-file-diff"></i>Changelogs 
+              <span class="badge rounded-pill bg-primary">
+                {{ changeLogCount }}
               </span>
             </button>
         </li>
@@ -16,6 +26,7 @@
     </div>
 
     <div class="tab-content" id="sysServicesTabContent">
+        <!-- Services Section -->
         <div class="tab-pane fade show active" id="systemServices" role="tabpanel" aria-labelledby="systemServices">
             <table class="table table-striped table-bordered table-sm table-hover nowrap"  id="systemServicesTable">
                 <thead>
@@ -46,7 +57,51 @@
                 </tbody>
             </table>
         </div>
+        <!-- Changelog Section -->
+        <div class="tab-pane fade" id="systemServicesChangelog" role="tabpanel" aria-labelledby="systemServicesChangelog">
+          <table class="table table-striped table-bordered table-sm" id="systemServicesChangelogTable">
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>Action</th>
+                <th>Service</th>
+                <th>Field</th>
+                <th>Previous Value</th>
+                <th>New Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="data in changeLogData" :key="data">
+                <td>
+                  {{ data.date }}
+                </td>
 
+                <td v-if="data.action == 'New'" style="color: green;">
+                  Install
+                </td>
+                <td v-if="data.action == 'Delete'" style="color:crimson">
+                  Delete
+                </td>
+                <td v-if="data.action == 'Update'" style="color: coral;">
+                  Update
+                </td>
+
+                <td>
+                  {{ data.service_name }}
+                </td>
+                <td>
+                  {{ data.field }}
+                </td>
+                <td style="color:crimson">
+                  {{ data.previous_value }}
+                </td>
+                <td style="color: green;">
+                  {{ data.new_value }}
+                </td>
+              </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
 </template>
 
@@ -88,6 +143,61 @@
             // Set Services Count
             this.systemServicesCount = this.systemServices.services.length;
 
+            this.changeLogData = this.changeLogData.map((item) => {
+            const date = formatToLocalTime(item.timestamp);
+            const changes = item.changes;
+
+            // Define the Action List
+            const actionList = [];
+            
+            // Find Deleted Services
+            if (changes.deleted_services) {
+              for (const service of changes.deleted_services) {
+                actionList.push({
+                  date,
+                  action: "Delete",
+                  service_name:  service.service_name,
+                  field: "-",
+                  previous_value: service,
+                  new_value: "-",
+                });
+              }
+            }
+
+            // Find Newly Added services
+            if (changes.new_services) {
+              for (const service of changes.new_services) {
+                actionList.push({
+                  date,
+                  action: "New",
+                  service_name:  service.service_name,
+                  field: "-",
+                  previous_value: "-",
+                  new_value: service,
+                });
+              }
+            }
+
+            // Find Updated services
+            if (changes.updated_services) {
+              for (const service_name in changes.updated_services) {
+                const serviceChanges = changes.updated_services[service_name];
+                for (const changeKey in serviceChanges) {
+                  actionList.push({
+                    date,
+                    action: "Update",
+                    service_name: service_name,
+                    field: changeKey,
+                    previous_value: serviceChanges[changeKey]["previous_value"],
+                    new_value: serviceChanges[changeKey]["new_value"],
+                  });
+                }
+              }
+            }
+
+            return actionList;
+          }).flat();
+
           } catch (error) {
             console.error(error);
           }
@@ -97,8 +207,5 @@
 </script>
 
 <style>
-.sysAppsActionIcons{
-  font-size: 2em;
-  max-width: 4rem;
-}
+
 </style>
