576d3cd8e99b6c4f1ee0585bbe3467a269733bf3
keraattin <kerem-oruc@outlook.com>
2023-12-07 20:07:24 +0300

    last logins module updated

--- Linux/Modules/last_logon.py
+++ Linux/Modules/last_logon.py
@@ -8,18 +8,19 @@ import json
 
 
 # Functions
-############################################################################## 
-last_logons = []
+##############################################################################
+def get_last_logons():
+    last_logons = []
 
-for line in subprocess.check_output(["last", "-w"]).decode("utf-8").splitlines():
-    fields = line.split()
-    if len(fields) >= 7:
-        user = fields[0]
-        last_logon = f"{fields[3]} {fields[4]} {fields[5]}"
-        last_logons.append({
-            "username": user, 
-            "last_logon": last_logon
-        })
+    for line in subprocess.check_output(["last", "-w"]).decode("utf-8").splitlines():
+        fields = line.split()
+        if len(fields) >= 7:
+            user = fields[0]
+            last_logon = f"{fields[3]} {fields[4]} {fields[5]}"
+            last_logons.append({
+                "username": user, 
+                "last_logon": last_logon
+            })
 
-return last_logons
+    return last_logons
 ##############################################################################
