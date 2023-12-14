0742fb33a64e8ecd937f6e2f6cc4bf9bfa6f1563
keraattin <kerem-oruc@outlook.com>
2023-12-14 21:37:58 +0300

    register agent success added

---
+++ Dashboard/Tests/Modules/Agent/__init__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+# Libraries
+##############################################################################
+import requests
+import json
+import random
+
+from Modules.Config import Urls,Endpoints
+##############################################################################
+
+# Config
+##############################################################################
+AGENT_REG_URL = Urls.base_url + Endpoints.agents_reg_ep
+##############################################################################
+
+
+# Test Agent Register Success
+##############################################################################
+def test_register_agent_success(token):
+    agent_types = [
+        'Windows',
+        'Linux',
+        'MacOs'
+    ]
+    
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': token
+    }
+
+    reg_data = {
+        'type': random.choice(agent_types)
+    }
+
+    response = requests.request(
+        "POST",
+        AGENT_REG_URL,
+        data=json.dumps(reg_data),
+        headers=headers
+    )
+
+    assert "Agent registered successfully." in response.text
+    assert response.status_code == 201
+
+    return True
+##############################################################################
