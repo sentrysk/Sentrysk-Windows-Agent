dafd4b18f65cb7129657dac1c16b26a090cbbbfa
keraattin <kerem-oruc@outlook.com>
2023-12-20 12:39:25 +0300

    Serialize method added to LogonData

--- Dashboard/Backend/SystemLastLogons/models.py
+++ Dashboard/Backend/SystemLastLogons/models.py
@@ -19,6 +19,16 @@ DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 class LogonData(EmbeddedDocument):
     username        = StringField()
     last_logon      = DateTimeField()
+
+    def serialize(self):
+        data = {
+            "username": self.username,
+            "last_logon": self.last_logon
+        }
+        return data
+
+    def __str__(self):
+        return str(self.serialize())
 ##############################################################################
 
 ##############################################################################
