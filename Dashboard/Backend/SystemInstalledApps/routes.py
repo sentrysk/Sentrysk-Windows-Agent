e353e47de8d0d7405ca7f242fac5e8e8e8947e17
keraattin <kerem-oruc@outlook.com>
2023-12-21 17:05:43 +0300

    Get All System Apps Serialization Method Changed

--- Dashboard/Backend/SystemInstalledApps/routes.py
+++ Dashboard/Backend/SystemInstalledApps/routes.py
@@ -26,17 +26,10 @@ sys_apps_bp = Blueprint('sys_apps_blueprint', __name__)
 @auth_token_required
 def get_all_system_apps():
     try:
+        # Get All System Apps from DB
         all_sys_apps = SystemInstalledApps.objects()
-
-        for sys_apps in all_sys_apps:
-            # Serialize "apps" section
-            apps = []
-            for app in sys_apps.apps:
-                apps.append(app.serialize())
-            # Append again apps to System apps
-            sys_apps.apps = apps
-        
-        return [info.serialize() for info in all_sys_apps] # Serialize & Return
+        # Serialize & Return
+        return [info.serialize() for info in all_sys_apps] 
     except Exception as e:
         return jsonify({"error":str(e)}), 500
 
