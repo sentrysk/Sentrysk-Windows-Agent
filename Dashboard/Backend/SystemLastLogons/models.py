364fdc66127cde2fca7ffab67584c552b11fa519
keraattin <kerem-oruc@outlook.com>
2023-12-21 16:38:24 +0300

    Serializing Added

--- Dashboard/Backend/SystemLastLogons/models.py
+++ Dashboard/Backend/SystemLastLogons/models.py
@@ -10,11 +10,7 @@ from Agents.models import Agent
 from datetime import datetime
 ##############################################################################
 
-# Global Values
-##############################################################################
-DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
-##############################################################################
-
+# LogonData Model
 ##############################################################################
 class LogonData(EmbeddedDocument):
     username        = StringField()
@@ -31,9 +27,26 @@ class LogonData(EmbeddedDocument):
         return str(self.serialize())
 ##############################################################################
 
+# SystemLastLogons Model
 ##############################################################################
 class SystemLastLogons(Document):
     agent               = ReferenceField(Agent)
     last_logons         = ListField(EmbeddedDocumentField(LogonData))
     updated             = DateTimeField(default=datetime.utcnow)
+
+    def serialize(self):
+        # Serialize All LogonData
+        serialized_logons = []
+        for logon in self.last_logons:
+            serialized_logons.append(logon.serialize())
+        
+        return {
+            "id":str(self.id),
+            "agent_id":str(self.agent.id),
+            "last_logons":serialized_logons,
+            "updated":self.updated
+        }
+
+    def __str__(self):
+        return str(self.serialize())
 ##############################################################################
