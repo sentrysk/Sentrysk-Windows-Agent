8633d423b7f7f8cadb4d2e1787dbf15c00ae82c5
keraattin <kerem-oruc@outlook.com>
2023-12-28 16:10:51 +0300

    User counts by agent id added

--- Dashboard/Backend/InformationalData/routes.py
+++ Dashboard/Backend/InformationalData/routes.py
@@ -4,6 +4,7 @@
 ##############################################################################
 from flask import Blueprint, request, jsonify
 
+from SystemUsers.models import SystemUsers
 
 ##############################################################################
 
@@ -15,6 +16,22 @@ inf_data_bp = Blueprint('informational_data', __name__)
 
 # Routes 
 ##############################################################################
-
-
+@inf_data_bp.route('/user_counts/<agent_id>', methods=['GET'])
+def get_sys_user_counts_by_agent_id(agent_id):
+    try:
+        # Get System Users
+        sys_users = SystemUsers.objects(agent=agent_id).first()
+        
+        # If exist
+        if sys_users:
+            return jsonify({
+                "user_counts":str(len(sys_users.users))
+            })
+        return jsonify({
+                "user_counts":0
+        })
+    except Exception as e:
+        return jsonify({
+                "user_counts":0
+        })
 ##############################################################################
