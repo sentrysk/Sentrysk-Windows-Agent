bc3c73aa6abcc09b0830ad632aa0bd6b5261fd96
keraattin <kerem-oruc@outlook.com>
2023-12-30 18:13:35 +0300

    Get Last Logons by ID Endpoint added

--- Dashboard/Backend/InformationalData/routes.py
+++ Dashboard/Backend/InformationalData/routes.py
@@ -7,7 +7,8 @@ from flask import Blueprint, jsonify
 from Shared.validators import auth_token_required
 from .functions import (
     get_sys_user_count_by_agent_id,
-    get_sys_user_changelog_entry_count_by_agent_id
+    get_sys_user_changelog_entry_count_by_agent_id,
+    get_last_logons_by_agent_id
 )
 ##############################################################################
 
@@ -38,6 +39,16 @@ def sys_user_changelog_count_by_agent_id(agent_id):
     
     return jsonify({
         "user_changelog_count": str(user_changelog_count)
+    })
+
+# Get Sys Users Last Logons Count by Agent ID
+@inf_data_bp.route('/user_last_logons_count/<agent_id>', methods=['GET'])
+@auth_token_required
+def sys_user_last_logons_count_by_agent_id(agent_id):
+    user_last_logons_count = get_last_logons_by_agent_id(agent_id)
+    
+    return jsonify({
+        "user_last_logons_count": str(user_last_logons_count)
     })
 
 ##############################################################################
