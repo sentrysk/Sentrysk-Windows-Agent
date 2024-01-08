370cbe223d9f43e563005399feff76f4f7e86dbf
keraattin <kerem-oruc@outlook.com>
2024-01-08 21:07:20 +0300

    Get Previous Sessions Endpoint Added

--- Dashboard/Backend/Session/routes.py
+++ Dashboard/Backend/Session/routes.py
@@ -2,9 +2,10 @@
 
 # Libraries
 ##############################################################################
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from Shared.validators import auth_token_required
+from .models import Session
 ##############################################################################
 
 # Blueprint
@@ -22,4 +23,22 @@ session_bp = Blueprint('session_blueprint', __name__)
 def check_session():
     return jsonify({"message":"Session active"}),200
 
+# Get Previous Session
+@session_bp.route('/prev_sessions', methods=['GET'])
+@auth_token_required
+def get_personal_prev_sessions():
+    # Get Token from Auth Header
+    auth_header = request.headers.get('Authorization')
+    # Get User ID from token
+    user_email = Session.objects(token=auth_header).first()['email']
+    # Get Sessions belongs to this
+    sessions = Session.objects(email=user_email, is_expired=True)
+    # Create Empty Previous Sessions List
+    prev_sessions = []
+    # Serialize the Sessions
+    for session in sessions:
+        prev_sessions.append(session.serialize())
+    
+    # Return the List of Previous Sessions
+    return jsonify(prev_sessions),200
 ##############################################################################
