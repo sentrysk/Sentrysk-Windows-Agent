459c14b16deb4b42f3c578ca70039874f9bfe52e
keraattin <kerem-oruc@outlook.com>
2024-01-09 11:17:23 +0300

    Get Last Dashboard Logins View Added

---
+++ Dashboard/Frontend/src/views/DashboardLastLoginsView.vue
@@ -0,0 +1,45 @@
+<template>
+    <Navbar /> <!-- Include the Navbar component here -->
+    <div class="container">
+        <table class="table table-striped table-bordered dt-responsive nowrap" id="agentsTable">
+          <thead>
+            <tr>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="lastLogin in lastLoginsData" :key="lastLogin.email">
+                <td>{{ lastLogin.created }}</td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+    
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import Navbar from '@/components/Navbar.vue';
+  import { getMyLastDashboardLogins } from '@/utils/requestUtils';
+  
+  export default {
+    name: 'DashboardLastLoginsView',
+    components: {
+      Navbar,
+    },
+    data(){
+        return{
+            lastLoginsData: {}
+        }
+    },
+    mounted(){
+        this.fillLastDasboardLogins();
+    },
+    methods:{
+        async fillLastDasboardLogins(){
+            this.lastLoginsData = await getMyLastDashboardLogins();
+        }
+    }
+  }
+  </script>
+  
